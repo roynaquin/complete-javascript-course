1421961c970e0fac8a64ef40b0f90198b94e9409
roynaquin <87366539+roynaquin@users.noreply.github.com>
2023-10-27 02:00:20 +0900

    Committing learning files for today

---
+++ 01-Fundamentals-Part-1/starter/script.js
@@ -0,0 +1,26 @@
+/*
+let js = 'amazing';
+if (js === 'amazing') alert('Javascript is Fun!');
+// console.log(40 + 8 + 23 - 10);
+console.log('Jonas');
+console.log(23);
+
+let firstName = 'Roy';
+
+console.log(firstName);
+console.log(firstName);
+console.log(firstName);
+
+// Variable name onventions
+let jonas_matilda = 'JM';
+let name = "jonas";
+
+let person = 'Jonas';
+let PI = 3.1415; //JS recognizs this. 
+
+//Make Variables descriptive so they are understood
+let myFirstJob = 'teacher';
+let mySecondJob = 'programmer';
+console.log('I started out as a ' + myFirstJob + ' before I became a ' + mySecondJob + '.');
+*/
+
