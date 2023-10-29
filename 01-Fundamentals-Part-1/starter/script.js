0851e268fd6e3185b14b00e95e1e48b8d498becf
roynaquin <87366539+roynaquin@users.noreply.github.com>
2023-10-29 15:31:35 +0900

    Daily Coding Practice

--- 01-Fundamentals-Part-1/starter/script.js
+++ 01-Fundamentals-Part-1/starter/script.js
@@ -1,7 +1,7 @@
 /*
 let js = 'amazing';
 if (js === 'amazing') alert('Javascript is Fun!');
-// console.log(40 + 8 + 23 - 10);
+console.log(40 + 8 + 23 - 10);
 console.log('Jonas');
 console.log(23);
 
@@ -22,7 +22,6 @@ let PI = 3.1415; //JS recognizs this.
 let myFirstJob = 'teacher';
 let mySecondJob = 'programmer';
 console.log('I started out as a ' + myFirstJob + ' before I became a ' + mySecondJob + '.');
-*/
 
 // Dynamic Typing Examples
 let javascriptIsFun = true;
@@ -44,3 +43,64 @@ year = 1991;
 console.log(typeof year);
 
 console.log(typeof null);
+
+
+// Video 12: let const and var
+//reassigning or mutating a variable
+let age = 30;
+age = 31;
+
+// const creates an immutable variable. By default use const
+const birthYear = 1991;
+
+//var is the old way of writinng ESC.
+var job = "progrrammer";
+job = "teacher";
+
+lastName = "Schmedtmann";
+console.log(lastName);
+
+
+//Video 13: Operators
+//Mathematic operators
+const now = 2037;
+const ageJonas = now - 1991;
+const ageSarah = now - 2020;
+console.log(ageJonas);
+console.log(ageSarah);
+
+console.log(ageJonas * 2, ageJonas / 10, 2 ** 3);
+// 2 ** 3 means 2 to the power of 3 = 2 * 2 * 2
+
+const firstName = "jonas";
+const lastName = "Schmedtmann";
+console.log(firstName + " " + lastName);
+
+//Assignment Operators
+let x = 10 + 5;
+x += 10; // x = x + 10 = 25
+x *= 4; // x = x + 4 = 100
+x /= 4; // x = x / 4 = 25
+x++; // x = x + 1 = 26
+x--; // x = x - 1 = 25
+console.log(x);
+
+// Comparison Operators
+console.log(ageJonas > ageSarah); // >, <, >=, <=
+console.log(ageSarah >= 18);
+const isFullAge = ageSarah >= 18;
+console.log(now - 1991 > now - 2018);
+
+// Operator Precedence
+const now = 2037;
+const ageJonas = now - 1991;
+const ageSarah = now - 2018;
+
+console.log(now - 1991 > now - 2018);
+let x, y;
+x = y = 25 - 10 - 5; // x = y = 10, x = 10
+console.log(x, y);
+
+const averageAge = (ageJonas + ageSarah) / 2;
+console.log(ageJonas, ageSarah, averageAge);
+*/
