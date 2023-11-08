0bbfef239e1365d264776e37afb4458de5c89597
roynaquin <87366539+roynaquin@users.noreply.github.com>
2023-11-09 01:47:56 +0900

    Commit evening code practice.

--- 05-Guess-My-Number/starter/script.js
+++ 05-Guess-My-Number/starter/script.js
@@ -1,1 +1,12 @@
 'use strict';
+//Selecting an element
+console.log(document.querySelector('.message').textContent);
+
+document.querySelector('.message').textContent = '😎 Correct Number!';
+console.log(
+  (document.querySelector('.message').textContent = '😎 Correct Number!')
+);
+document.querySelector('.number').textContent = 13;
+document.querySelector('.score').textContent = 10;
+document.querySelector('.guess').value = 12;
+document.querySelector('.number').textContent = '❓';
